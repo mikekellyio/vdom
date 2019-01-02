d3c4c6cd9a0b44a55a1ac7a53eabf986372efb50
Mike Kelly <mkelly@excivity.com>
2019-01-02 15:29:50 -0500

    implement diffing for elements and strings

--- js/diff.js
+++ js/diff.js
@@ -1,21 +1,44 @@
+import render from "./render";
 const diff = (oldVTree, newVTree) => {
-  //newVtree is undefined
-  //then just remove the $node passing into the patch then
+  if (newVTree === undefined) {
+    //newVtree is undefined
+    //then just remove the $node passing into the patch then and return undefined
+    return $node => {
+      $node.remove();
+      return undefined;
+    };
+  }
 
-  //old and new are both strings
-  //same string?
-  //then noop
-  //else replace $node with render(newVTree)
+  if (typeof oldVTree === "string" || typeof newVTree === "string") {
+    if (oldVTree !== newVTree) {
+      //could be 2 cases
+      //1. both trees are strings and unequal
+      //2. one is string, the other is elements
+      //either case, render(newVTree)
+      return $node => {
+        const $newNode = render(newVTree);
+        $node.replaceWith($newNode);
+        return $newNode;
+      };
+    } else {
+      return $node => $node;
+    }
+  }
 
-  //one is string, other is element node
-  //then replace $node with render(newVTree)
+  if (oldVTree.tagName !== newVTree.tagName) {
+    // assume if tag doesn't match, they are completely different trees
+    // and simply render the newVTree
+    return $node => {
+      const $newNode = render(newVTree);
+      $node.replaceWith($newNode);
+      return $newNode;
+    };
+  }
 
-  //tagName's aren't ===
-  //then replace $node with render(newVTree)
-
-  return $el => {
-    return $el;
-  };
+  // If the code reaches (A), it implies the following:
+  // 1. oldVTree and newVTree are both virtual elements.
+  // 2. They have the same tagName.
+  // 3. They might have different attrs and children.
 };
 
 export default diff;
