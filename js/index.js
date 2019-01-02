fa06b971791b469f22b711d2d40d8a8a671383b7
Mike Kelly <mkelly@excivity.com>
2019-01-02 13:58:19 -0500

    nested create elements

--- js/index.js
+++ js/index.js
@@ -5,6 +5,13 @@ import createElement from "./createElement";
 // $ - when referring to real doms, e.g. $div, $el, $app
 // v - when referring to virtual doms, e.g. vDiv, vEl, vApp
 
-const vApp = createElement("div", { attrs: { id: "app" } });
+const vApp = createElement("div", {
+  attrs: { id: "app" },
+  children: [
+    createElement("img", {
+      attrs: { src: "https://mkio.link/easy" }
+    })
+  ]
+});
 
 console.log(vApp);
