9616e71c078e65c9a41a5eddd36e93d013448db4
Mike Kelly <mkelly@excivity.com>
2019-01-02 12:36:11 -0500

    implement in index

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 import "../css/index.scss";
 
+import createElement from "./createElement";
+
 // $ - when referring to real doms, e.g. $div, $el, $app
 // v - when referring to virtual doms, e.g. vDiv, vEl, vApp
 
-const vApp = {
-  tagName: "div",
-  attrs: {
-    id: "app"
-  }
-};
+const vApp = createElement("div", { attrs: { id: "app" } });
 
 console.log(vApp);
