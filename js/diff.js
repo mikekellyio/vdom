48bc8a44bab52935138782131111775059c08fcf
Mike Kelly <mkelly@excivity.com>
2019-01-02 15:10:42 -0500

    diff scaffolding w/ comments

--- js/diff.js
+++ js/diff.js
@@ -1,4 +1,18 @@
-const diff = (a, b) => {
+const diff = (oldVTree, newVTree) => {
+  //newVtree is undefined
+  //then just remove the $node passing into the patch then
+
+  //old and new are both strings
+  //same string?
+  //then noop
+  //else replace $node with render(newVTree)
+
+  //one is string, other is element node
+  //then replace $node with render(newVTree)
+
+  //tagName's aren't ===
+  //then replace $node with render(newVTree)
+
   return $el => {
     return $el;
   };
