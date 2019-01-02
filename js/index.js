7a7f14c2db81806777e2cfec190bf36ab554ab9a
Mike Kelly <mkelly@excivity.com>
2019-01-02 14:45:30 -0500

    make app dynamic updating

--- js/index.js
+++ js/index.js
@@ -7,17 +7,26 @@ import render from "./render";
 // $ - when referring to real doms, e.g. $div, $el, $app
 // v - when referring to virtual doms, e.g. vDiv, vEl, vApp
 
-const vApp = createElement("div", {
-  attrs: { id: "app" },
-  children: [
-    "That was easy",
-    createElement("img", {
-      attrs: { src: "https://mkio.link/easy" }
-    })
-  ]
-});
+const createVApp = count =>
+  createElement("div", {
+    attrs: { id: "app", dataCount: count },
+    children: [
+      "That was easy",
+      createElement("img", {
+        attrs: { src: "https://mkio.link/easy" }
+      }),
+      createElement("br"),
+      "The current count is: ",
+      String(count)
+    ]
+  });
 
+let count = 0;
+const vApp = createVApp(count);
 const $app = render(vApp);
-console.log($app);
+let $rootEl = mount($app, document.getElementById("app"));
 
-mount($app, document.getElementById("app"));
+setInterval(() => {
+  count++;
+  $rootEl = mount(render(createVApp(count)), $rootEl);
+}, 1000);
